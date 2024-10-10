fa436838879f4b34d25f971087644ce4c1138100
ABHAY CHAVAN <75495921+abhay113@users.noreply.github.com>
2024-10-10 11:26:07 +0530

    2nd time

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostListComponent } from './components/post-list/post-list.component';
 import { PostDetailComponent } from './components/post-detail/post-detail.component';
-import { PostCreateComponent } from './components/post-create/post-create.component';
+import { PostCreateComponent } from './components/post-create/post-create.component';  // Make sure PostCreateComponent is imported
 import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostListComponent,
     PostDetailComponent,
-    PostCreateComponent
+    PostCreateComponent, 
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,HttpClientModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/post-create/post-create.component.ts
+++ src/app/components/post-create/post-create.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
-  styleUrl: './post-create.component.css'
+  styleUrls: ['./post-create.component.css']
 })
-export class PostCreateComponent {
+export class PostCreateComponent implements OnInit {
+  postForm!: FormGroup;  // Ensure postForm is defined using the `!` operator for strict mode
 
+  ngOnInit() {
+    // Initialize the form
+    this.postForm = new FormGroup({
+      title: new FormControl(null, [Validators.required, Validators.minLength(3)]),
+      content: new FormControl(null, [Validators.required, Validators.minLength(10)])
+    });
+  }
+
+  onSubmit() {
+    if (this.postForm.valid) {
+      console.log(this.postForm.value);
+      // Handle the form submission here
+    }
+  }
 }
